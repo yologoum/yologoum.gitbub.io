c5216e3a23d1d7a51299dcc943754f81babffa8a
이경미 <goum@activar.co.kr>
2017-05-18 18:53:56 +0900

    소스 작업

---
+++ js/temp.js
@@ -0,0 +1,28 @@
+// 홍길동  2017-01-01
+
+var 설정 값 = 0;
+
+common();
+
+$(선택자).이벤트
+
+$(window).scroll(function(){
+
+    common();
+
+});
+
+$(window).resize(function(){
+
+    common();
+
+});
+
+/*
+ Description : 설명
+ Revisions
+    - 2017-01-01  홍길동  수정내용
+ */
+ function common() {
+
+ }
--- js/작성요령.js
+++ js/작성요령.js
@@ -1,98 +1,36 @@
-js 작성법
+js 작성
 
-자바스트립
+파일은 페이지별로 분리하며 공통스크립트는 common폴더에 정리한다.
+수정, 관리를 위해 ????? 함수로 분리하여 작성
 
+body 하단에 스크립트 링크 연결 (공통 사용인 플러그인 및 제이쿼리는 예외 head에 유지)
 
-이벤트 >
-
-$(document).ready(function(){
-
-}
-
-$(function(){
-
-})
-
-}
-
-윈도우
-도큐멘트
-
-문서다 다 읽으면
+작성 순서
+01. 설정 == 변수, 배열, 위치
+02. 함수 실행
+03. scroll/resize/이벤트
+04. 함수
 
 
-이벤트
-
-문서를 다 읽으면 (){
-    01. 설정 == 변수, 배열, 위치.....
-    02. 셋팅 == 기능셋팅.. 마우스 이벤트...
-    03. 함수 == 해야할일
-    //04. 프로토타입 ==
-}
-
-
-
-문서를 다 읽으면 (){
-
-
-    var aaa= 100;
-
-
-
-
-    $('.family_site ul li a').on('click', function(){
-        할일1();
+on메소드 사용법
+    각 이벤트에 따른 함수를 사용할 수 있다.
+    $('.box1').on({
+       mouseenter : function(){
+           $(this).css('background','skyblue');
+       },
+       mouseleave : function(){
+           $(this).css('background','darkblue');
+       },
+       click : function(){
+           $(this).css('width',500);
+       }
     });
 
-    $('.family_site ul li a').on('click', function(){
-        할일2();
-    });
+    한 선택자에 다수의 이벤트를 연결할 수 있다.
+   $(선택자).on('click mouseenter', function(){
+       일어날 일
+   });
 
-    $('.family_site ul li a').on('click', function(){
-        할일3();
-    });
-
-
-
-
-
-    할일1(){
-
-    }
-
-    할일2(){
-
-    }
-
-    할일3(){
-
-    }
-
-
-
-    var aaa= 100;
-    var aaa= 100;
-    var aaa= 100;
-
-    할일1(){
-
-    }
-
-
-
-function name(){
-
-}
-
-var name = function(){
-
-}
-
-
-selecter.on('click'){
-
-}
-
-slelecter.click
-
-add
+레디 함수 구분
+$(document).ready(function(){ ... }); === $(function(){ ... });   //문서로드후 실행
+(function($){ ... })(jQuery);   //$의 의미를 jquery에서 사용하겠다는 의미(다른 라이브러리에서도 사용할수 있기에 jquery 객체화 시키는것)
